4f0a268c44aa6516037c9bb327898af62a23364c
jaeyeon kim <jakind@naver.com>
2023-07-24 16:01:02 +0900

    Initial commit from gatsby: (https://github.com/devHudi/gatsby-starter-hoodie.git)

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,3 @@
+## What does this PR do?
+
+closes #ISSUE_NUMBER
---
+++ src/assets/theme/index.js
@@ -0,0 +1,86 @@
+const GRAY9 = "#191919"
+const GRAY8 = "#2D2D2D"
+const GRAY7 = "#404040"
+const GRAY6 = "#868e96"
+const GRAY5 = "#adb5bd"
+const GRAY4 = "#ced4da"
+const GRAY3 = "#dee2e6"
+const GRAY2 = "#e9ecef"
+const GRAY1 = "#f1f3f5"
+const GRAY0 = "#f8f9fa"
+
+export const light = {
+  name: "light",
+  colors: {
+    bodyBackground: "#ffffff",
+    text: GRAY9,
+    secondaryText: GRAY7,
+    tertiaryText: GRAY6,
+    mutedText: GRAY5,
+    hoveredLinkText: GRAY0,
+    border: GRAY4,
+    activatedBorder: GRAY6,
+    background: GRAY1,
+    icon: GRAY6,
+    divider: GRAY2,
+    headerBackground: "rgba(255, 255, 255, 0.85)",
+    headerShadow: "rgba(0, 0, 0, 0.08)",
+    inlineCodeBackground: GRAY2,
+    inlineCodeBackgroundDarker: GRAY4,
+    tagBackground: GRAY1,
+    selectedTagBackground: GRAY7,
+    hoveredTagBackground: GRAY3,
+    hoveredSelectedTagBackground: GRAY8,
+    nextPostButtonBackground: "rgba(0, 0, 0, 0.06)",
+    hoveredNextPostButtonBackground: "rgba(0, 0, 0, 0.08)",
+    seriesBackground: GRAY1,
+    tagText: GRAY7,
+    selectedTagText: GRAY0,
+    spinner: GRAY7,
+    scrollTrack: GRAY1,
+    scrollHandle: GRAY4,
+    blockQuoteBorder: GRAY4,
+    blockQuoteBackground: GRAY1,
+    textFieldBorder: GRAY4,
+    textFieldActivatedBorder: GRAY5,
+    tableBackground: GRAY1,
+  },
+}
+
+export const dark = {
+  name: "dark",
+  colors: {
+    bodyBackground: GRAY9,
+    text: GRAY0,
+    secondaryText: GRAY4,
+    tertiaryText: GRAY5,
+    mutedText: GRAY6,
+    hoveredLinkText: GRAY9,
+    border: GRAY5,
+    activatedBorder: GRAY3,
+    background: GRAY8,
+    icon: GRAY5,
+    divider: GRAY8,
+    headerBackground: "rgba(25, 25, 25, 0.85)",
+    headerShadow: "rgba(150, 150, 150, 0.08)",
+    inlineCodeBackground: GRAY8,
+    inlineCodeBackgroundDarker: GRAY7,
+    tagBackground: GRAY8,
+    selectedTagBackground: GRAY2,
+    hoveredTagBackground: GRAY7,
+    hoveredSelectedTagBackground: GRAY1,
+    nextPostButtonBackground: "rgba(255, 255, 255, 0.05)",
+    hoveredNextPostButtonBackground: "rgba(255, 255, 255, 0.08)",
+    seriesBackground: GRAY8,
+    tagText: GRAY2,
+    selectedTagText: GRAY9,
+    spinner: GRAY1,
+    scrollTrack: GRAY8,
+    scrollHandle: GRAY7,
+    blockQuoteBorder: GRAY7,
+    blockQuoteBackground: GRAY8,
+    textFieldBorder: GRAY7,
+    textFieldActivatedBorder: GRAY6,
+    tableBackground: "#292e33",
+  },
+}
