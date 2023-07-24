5f19b8733fcd98dd21537c0c15fda3e48f6de4cb
jaeyeon kim <jakind@naver.com>
2023-07-24 17:37:31 +0900

    chore: blog-config.js 추가

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "괜찮을지도",
+  description: "안녕하세요! 괜찮을지도 기술블로그입니다.",
+  author: "괜찮을지도",
+  siteUrl: "https://map-befine-official",
+  links: {
+    github: "https://github.com/woowacourse-teams/2023-map-befine",
+  },
+  utterances: {
+    repo: "devHudi/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
+
