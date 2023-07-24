8a2e1dc63555020df420a4107be7d1236448c222
jaeyeon kim <jakind@naver.com>
2023-07-24 18:02:28 +0900

    fix: siteUrl 수정

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "괜찮을지도",
   description: "안녕하세요! 괜찮을지도 기술블로그입니다.",
   author: "괜찮을지도",
-  siteUrl: "https://map-befine-official",
+  siteUrl: "https://map-befine-official.github.io",
   links: {
     github: "https://github.com/woowacourse-teams/2023-map-befine",
   },
